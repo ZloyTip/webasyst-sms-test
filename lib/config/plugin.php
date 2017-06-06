bc42e89da114e1090ec7726170b0a34f6e8cf89f
Eugen <jk@47.kiev.ua>
2017-06-06 11:25:45 +0300

    config.php and .htaccess

---
+++ lib/config/plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'name'        => /*_wp*/('TEST'),
+    'description' => /*_wp*/('Webasyst SMS test'),
+    'icon'        => 'img/test.png',
+    'vendor'      => '972539',
+    'version'     => '1.0',
+);
